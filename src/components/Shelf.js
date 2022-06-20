3d4129e986fbf6ab7c597f4e79c2f2d6f7ad0dd3
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2022-06-20 03:25:59 +0200

    rendering list of authors for single book

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -4,7 +4,7 @@ function Shelf({ title, books }) {
     <div className="shelf-container">
       <div className="head">
         <h4 className="title me-2">{title}</h4>
-        <span>( 4 items )</span>
+        <span>( {books.length} items )</span>
       </div>
       <div className="books-container">
         <div className="row g-5">
